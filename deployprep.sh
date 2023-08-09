1cf3b92867d896a6e68ad9a0896379ba3d7d7ada
Nic Barr <1134853+theothernic@users.noreply.github.com>
2023-08-09 18:21:17 -0500

    more package script work

--- deployprep.sh
+++ deployprep.sh
@@ -12,6 +12,10 @@ if [ -d node_modules/ ]; then
   rm -rf node_modules/;
 fi
 
+rm -f .editorconfig
+rm -f .gitattributes
+rm -f .gitignore
+rm -f README.md
 rm -f package.json
 rm -f package-lock.json
 
